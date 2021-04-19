d691b5f2dd1f0d1f2616701898f2edbb0be5120e
Livia Nascimento <livcristinna@gmail.com>
2021-04-19 14:57:39 -0300

    1th day

---
+++ podcastr/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //Integração do react com a web (browser)
+import App from './App';
+
+
+ReactDOM.render( //Serve para renderizar essa função app
+
+    <App />, //Executa essa funçãoo buscando ela no hoot
+    
+ 
+  document.getElementById('root')
+);
+
