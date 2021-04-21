7c3e06f382cf645f0e6aa00a270513cfba144de5
Livia Nascimento <livcristinna@gmail.com>
2021-04-21 01:20:35 -0300

    2th day

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -6,3 +6,17 @@ export default function Home() {
     </div>
   );
 }
+
+
+//SSG
+export async function getStaticProps(){
+  const response = await fetch ('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8, //Isso aqui serve para definir que a página será atualizada de 8 em 8 horas.
+  }
+}
