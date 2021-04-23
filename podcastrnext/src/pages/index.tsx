829fe44189ed912494edb7ed212d776ea005409c
Livia Nascimento <livcristinna@gmail.com>
2021-04-22 22:55:52 -0300

    3th day

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,22 +1,21 @@
-import { GetStaticProps } from 'next';
-import { api } from '../services/api';
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
 
 type Episode = {
-  id: string,
-  title: string,
-  members: string,
-  published_at: string
+  id: string;
+  title: string;
+  members: string
 }
 type HomeProps = {
-  episodes: Episode[], // Dizer que tenho um array de episodes
+  episodes: Episode[];
 }
-
 
 export default function Home(props: HomeProps) {
   return (
     <div>
-    index
+    <h1> index</h1>
+    <p> {JSON.stringify(props.episodes)}</p>
     </div>
   );
 }
@@ -24,17 +23,7 @@ export default function Home(props: HomeProps) {
 
 //SSG
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get ('episodes', {
-    params: {
-      _limit: 12,
-      _sort: 'published_at',
-      _order: 'desc'
-
-    }
-
-
-  });
-  
+  const {data} = await api.get('http://localhost:3333/episodes');
 
   return {
     props: {
